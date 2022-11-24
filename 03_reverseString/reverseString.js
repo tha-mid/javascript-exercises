8714e078bf49bfbe1fde1e3546e98d2477c038e6
Midori <thaistsushima@gmail.com>
2022-11-24 16:17:00 -0300

    Resolved exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let revString = '';
 
+    for (i = 1; i <= string.length; i++) {
+        revString += string[string.length - i];
+    }
+
+    return revString;
 };
 
 // Do not edit below this line

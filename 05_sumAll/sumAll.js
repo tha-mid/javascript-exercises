846eea68026b0ea71fbac511478a6ed0bd6fc2cf
Midori <thaistsushima@gmail.com>
2022-12-07 10:32:37 -0300

    Resolved exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    let higher = Math.max(val1, val2);
+    let lower = Math.min(val1, val2);
+    let total = 0;
 
+    if (val1 < 0 || val2 < 0 || !Number.isInteger(val1) || !Number.isInteger(val2)) {
+        return 'ERROR';
+    }
+
+    for (i = lower; i <= higher; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line

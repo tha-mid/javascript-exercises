b229e2c82a429d54e221ec1d3de1f4a0e1fce3cb
Midori <thaistsushima@gmail.com>
2022-12-07 11:14:47 -0300

    Resolved exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if (year%4 == 0) {
+        if (year%100 == 0 && year%400 == 0) {
+            return true;
+        } else if (year%100 == 0 && !year%400 == 0) {
+            return false;
+        }
+
+        return true;
+
+    } else return false;
 
 };
+
+//THE ODIN PROJECT SOLUTION
+// const leapYears = function(year) {
+//     return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+//   };
+
 
 // Do not edit below this line
 module.exports = leapYears;

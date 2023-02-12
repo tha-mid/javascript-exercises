fc40e5ef9885e452c0f889152d82ebf0ebd3aa47
Midori <thaistsushima@gmail.com>
2023-02-12 15:54:41 -0300

    Resolved exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) return 'OOPS';
+    if (num === 0) return 0;
+
+    let fibonacci = [1, 1];
+
+    for (let i = 3; i <= num; i++) {
+        fibonacci.push(fibonacci[fibonacci.length-1] + fibonacci[fibonacci.length - 2]);
+    }
+
+    return fibonacci[fibonacci.length - 1];
 };
 
 // Do not edit below this line

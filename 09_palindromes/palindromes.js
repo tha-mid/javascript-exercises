680ac70255b3803748e7cde265b5febd81b509a3
Midori <thaistsushima@gmail.com>
2023-02-12 14:54:36 -0300

    Resolved exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stringConverted = string.toLowerCase().replace(/[^a-z]/g, '');
 
+    return stringConverted
+                        .split('')
+                        .reverse()
+                        .join('') === stringConverted;
 };
 
 // Do not edit below this line

a7fceab62a220b1b3a34e729af40bb466d8675d4
Midori <thaistsushima@gmail.com>
2023-02-13 14:12:47 -0300

    Resolved exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    const objSorted = obj.sort((a, b) => getAge(a) < getAge(b) ? 1 : -1);
+    return objSorted[0];
 };
+
+function getAge (person) {
+
+    if (!person.yearOfDeath) {
+        person.yearOfDeath = (new Date()).getFullYear();
+    }
+    
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
